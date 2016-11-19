b83f48792bc89954028d6d2df9b803385d85ba2b
Maxwell Reeser <maxwellr96@gmail.com>
2016-11-19 17:05:37 -0600

    Update WeatherMonitor.java

--- src/eden/WeatherMonitor.java
+++ src/eden/WeatherMonitor.java
@@ -6,6 +6,14 @@
 package eden;
 
 import eden.AlertType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+import javafx.stage.Stage;
 
 /**
  *
