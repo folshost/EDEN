7eabddb4993a7d210ee2ded8bba2a909c89c27fa
lhymel9 <laynehy@gmail.com>
2016-11-20 15:55:35 -0600

    Updated PlotDirectory.java, EDEN.java (#11)
    -Added an iterator that creates buttons based on the elements in the
    ArrayList<Plot>.
    -Added a way to display images based on the button that was pressed.
    -Included dummy images and plots to test EDEN.java (Remember to set the
    correct path in EDEN and PlotDirectory!)
    
    +What's needed now are the methods that display the ActivePlot...

--- src/eden/EDEN.java
+++ src/eden/EDEN.java
@@ -2,6 +2,7 @@
 package eden;
 
 import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -13,7 +14,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.text.Text;
@@ -26,59 +26,16 @@ import javafx.scene.paint.Color;
  */
 public class EDEN extends Application {
 	PlotDirectory pD = new PlotDirectory();
-	Button btn1, btn2, btn3, btn4, btn5, btn6, btn7;
 	ImageView imgV;
+	Button btnI;
     
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
-	AlertType alertType = WeatherMonitor.main();
-        if(alertType != null){
-            Stage stage = new Stage();
-            Group rootGroup = new Group();               
-            Scene alertScene = new Scene(rootGroup, 300, 250,Color.GOLD);        
-            Text text = new Text(25, 50, "Extreme weather has been detected"
-                + "in you area!\n\n\t\tIt is of the type:\n\t\t" + 
-                alertType.getTypeDescrip());
-        
-            rootGroup.getChildren().add(text);
-        
-            stage.setTitle("Alert!");
-            stage.setScene(alertScene);
-            stage.show();
-        }    
     	PlotDirectory pD = new PlotDirectory();
     	pD.loadPlotList();
+    	Button btn;
     	
-    	if (pD.getList().get(0) != null)
-    		btn1 = new Button(pD.getList().get(0).getName());
-    	else
-    		btn1 = new Button("Empty");
-    	if (pD.getList().get(1) != null)
-    		btn2 = new Button(pD.getList().get(1).getName());
-    	else
-    		btn2 = new Button("Empty");
-    	if (pD.getList().get(2) != null)
-    		btn3 = new Button(pD.getList().get(2).getName());
-    	else
-    		btn3 = new Button("Empty");
-    	if (pD.getList().get(3) != null)
-    		btn4 = new Button(pD.getList().get(3).getName());
-    	else
-    		btn4 = new Button("Empty");
-    	if (pD.getList().get(4) != null)
-    		btn5 = new Button(pD.getList().get(4).getName());
-    	else
-    		btn5 = new Button("Empty");
-    	if (pD.getList().get(5) != null)
-    		btn6 = new Button(pD.getList().get(5).getName());
-    	else
-    		btn6 = new Button("Empty");
-    	if (pD.getList().get(6) != null)
-    		btn7 = new Button(pD.getList().get(6).getName());
-    	else
-    		btn7 = new Button("Empty");
-    	
-    	HBox btnBox = new HBox(btn1, btn2, btn3, btn4, btn5, btn6, btn7);
+    	HBox btnBox = new HBox();
     	btnBox.setSpacing(20);
     	btnBox.setPadding(new Insets(20));
     	imgV = new ImageView();
@@ -89,36 +46,58 @@ public class EDEN extends Application {
     	root.setTop(btnBox);
     	root.setCenter(imgV);
     	
-    	Scene scene = new Scene(root, 600, 600);
+    	for(int i = 0; i <= pD.getList().size() - 1; i++) {
+    			btn = new Button(pD.getList().get(i).getName());
+    			btnBox.getChildren().addAll(btn);
+    			btn.setOnAction(e->{
+					try {
+						handleButtonAction(e);
+						root.setBottom(btnI);
+					} catch (MalformedURLException e1) {
+						e1.printStackTrace();
+					}
+				});
+    	}
+    	
+    	Scene scene = new Scene(root, 500, 600);
     	primaryStage.setTitle("EDEN");
     	primaryStage.setScene(scene);
     	primaryStage.show();
+    	
+    	AlertType alertType = WeatherMonitor.main();
+    	if(alertType != null){
+            Stage stage = new Stage();
+            Group rootGroup = new Group();               
+            Scene alertScene = new Scene(rootGroup, 300, 250,Color.GOLD);        
+            Text text = new Text(25, 50, "Extreme weather has been detected"
+                + " in you area!\n\n\t\tIt is of the type:\n\t\t" + 
+                alertType.getTypeDescrip());
+        
+            rootGroup.getChildren().add(text);
+        
+            stage.setTitle("Alert!");
+            stage.setScene(alertScene);
+            stage.show();
+        } 
     }
     
-    //private void handleButtonAction(ActionEvent event) {
-    	//Image img;
-    	
-    	//if(event.getSource() == btn1)
-    		//img = new Image("path/to/image" + pD.getList().get(0).getName());
-    	//else if(event.getSource() == btn2)
-    		//img = new Image("path/to/image" + pD.getList().get(1).getName());
-    	//else if(event.getSource() == btn3)
-    		//img = new Image("path/to/image" + pD.getList().get(2).getName());
-    	//else if(event.getSource() == btn4)
-    		//img = new Image("path/to/image" + pD.getList().get(3).getName());
-    	//else if(event.getSource() == btn5)
-    		//img = new Image("path/to/image" + pD.getList().get(4).getName());
-    	//else if(event.getSource() == btn6)
-    		//img = new Image("path/to/image" + pD.getList().get(5).getName());
-    	//else if(event.getSource() == btn7)
-    		//img = new Image("path/to/image" + pD.getList().get(6).getName());
-    	//else
-    		//img = null;
-    	
-    	//imgV.setImage(img);
-    //}
-    
-    public static void main(String[] args) {
+    private void handleButtonAction(ActionEvent e) throws MalformedURLException {
+		Image img;
+		Button b = (Button) e.getSource();
+		String path = "file:C:\\Users\\Layne\\workspace\\EDEN\\plots\\" + b.getText() + ".bmp";
+		img = new Image(path);
+		imgV.setImage(img);
+		btnI = new Button("Load");
+		btnI.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("This should load " + b.getText() + ".");
+            }
+        });
+	}
+
+	public static void main(String[] args) {
         launch(args);
     }
     
